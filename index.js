32eb0eca3e8a0988a8dc690f0196640a163b4027
Reed Martin <reedhmartin@gmail.com>
2023-04-01 12:29:45 -0400

    Updated write function error handling. Added comments to index.js.

--- index.js
+++ index.js
@@ -5,66 +5,84 @@ const fs = require('fs');
 
 // TODO : Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a brief description of your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Provide installation instructions:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide usage information:',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Provide contribution guidelines:',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Provide test instructions:',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None'],
-    },
-    {
-      type: 'input',
-      name: 'githubUsername',
-      message: 'Enter your GitHub username:',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:',
-    },];
+  // Project title
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  // Project description
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of your project:',
+  },
+  // Installation instructions
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  // Usage information
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  // Contribution guidelines
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  // Test instructions
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  // License selection
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None'],
+  },
+  // GitHub username
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  // Email address
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
 
 // TODO : Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFileSync(fileName, data, (err) =>
-    err ? console.error(err) : console.log('Successfully created README file!')
-  );
+  // Write data to the specified file
+  fs.writeFile(fileName, data, (err) => {
+    // If there is an error, log it
+    if (err) {
+      console.error(err);
+    } else {
+      // If successful, inform the user
+      console.log('Successfully created README file!');
+    }
+  });
 }
 
-// TODO: Create a function to initialize app
+// TODO : Create a function to initialize app
 function init() {
+  // Prompt the user with questions and get their input
   inquirer.prompt(questions).then((answers) => {
-    // Generate the README content using the user input (You'll need to create a generateMarkdown function in the `generateMarkdown.js` file)
-    // const content = generateMarkdown(answers);
-    // For now, let's just create a placeholder content
+    // Convert answers to a JSON-formatted string (Replace this with generateMarkdown function call later)
+
     const content = JSON.stringify(answers, null, 2);
     
     // Write the content to a README file
